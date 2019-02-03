0282667551175ca5d1d35c2ad181f527273e14b9
Theark-Leonidas <45110734+Theark-Leonidas@users.noreply.github.com>
2019-02-03 05:32:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const  Discord  =  require ( ' discord.js ' );
+const  bot  =  new  Discord.Client ();
+ jeton  const =  processus . env . jeton 
+préf  préfixe  = ( " $ " );
+
+client.login("NTQxNDQwNjcxNzQ4MzI1Mzc4.Dzfnpg.2Z1Nga9RtyLZVAl9LGnkgTXajR8")
